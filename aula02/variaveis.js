2b899b5d6ec8371c987ec69b0ca0ba0683d164bb
Gabi Urbano <gabi.urbano182@gmail.com>
2024-03-07 10:19:33 -0300

    Aula sobre varíáveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,25 @@
+var numero = 2;
+let texto = "hello";
+const oQueEisso = "?";
+// o ponto e virgula é opcional! boas práticas o usam ♥ 
+
+console.log("Tipo var:", numero);
+// concatenar com virgula me garante um espaço
+
+numero = 13; 
+
+console.log(texto + " o número agora é " + numero);
+
+
+texto = 69; 
+
+console.log(texto + numero);
+// concatenar com + dois valores numericos vai acabar somando
+
+// console.log(oQueEisso);
+
+// constantes nao podem ter novos valores assinalados a ela 
+
+// oQueEisso = "uma constante????";
+
+// console.log(oQueEisso)
