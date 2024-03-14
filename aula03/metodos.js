17832d64e819e3465a2927d560d1770db1e56ba7
Gabi Urbano <gabi.urbano182@gmail.com>
2024-03-14 08:31:20 -0300

    aula sobre métodos e funções

---
+++ aula03/metodos.js
@@ -0,0 +1,11 @@
+let txt = "Hello migs";
+
+console.log(txt.length);
+
+console.log(txt.slice(6, 9));
+
+console.log(txt.replace("Hello migs", "Siles maior divo"));
+
+let newTxt = console.log(txt.toUpperCase);
+
+console.warn(newTxt.toUpperCase());
